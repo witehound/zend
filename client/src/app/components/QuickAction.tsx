36b3b826001fc132db5c75a7848b6132ece9fb8c
witehound <netvid00@gmail.com>
2023-07-23 23:55:12 +0100

    fixed quickaction border radius

--- client/src/app/components/QuickAction.tsx
+++ client/src/app/components/QuickAction.tsx
@@ -1,6 +1,6 @@
 export default function QuickAction() {
   return (
-    <main className="bg-black flex rounded-sm p-2 justify-evenly">
+    <main className="bg-black flex rounded-[5px] p-2 justify-evenly">
       <p>send</p>
       <p>send</p>
     </main>
